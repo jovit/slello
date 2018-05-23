938c483ea0f0724da594f9fe8d933f0a1beb1c76
João Vitor Araki Gonçalves <joao.goncalves@movile.com>
2018-05-22 22:11:44 -0300

    exemple

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+import ProjectStatus from "./screens/ProjectStatus";
+
 export default class App extends Component {
   render() {
     return (
       <div>
         <BrowserRouter>
-          <Switch> </Switch>
+          <Switch> 
+            <Route component={ProjectStatus} path="/status" />
+          </Switch>
         </BrowserRouter>
       </div>
     );
---
+++ src/screens/ProjectStatus/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Button } from 'semantic-ui-react'
+
+export default class ProjectStatus extends React.Component {
+  render() {
+  return (
+        <div>Hello World
+            <Button>Click me</Button>
+        </div>
+    );
+  }
+}
