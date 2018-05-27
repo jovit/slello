177526ee458bac77a638824b4408976eaa525aa0
LuizGuilherme <gui.carvalho1905@gmail.com>
2018-05-27 02:38:54 -0300

    Historico

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-
 import ProjectStatus from "./screens/ProjectStatus";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
-          <Switch> 
-            <Route component={ProjectStatus} path="/status" />
-          </Switch>
-        </BrowserRouter>
+        { <BrowserRouter>
+           <Switch>
+            <Route component={ProjectStatus} path="/historico" />
+           </Switch>
+         </BrowserRouter> }
       </div>
     );
   }
