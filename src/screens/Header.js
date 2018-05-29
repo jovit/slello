37ac9c6fd60b4baeec5399550358b899136ab138
João Victor Fernandes <ra158043@students.ic.unicamp.br>
2018-05-29 17:03:07 -0300

    Chat fix finished

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -4,6 +4,13 @@ const Header = () => (
 
   <div class="header">
     <h1 class="header__title">Slello</h1>
+    <div class="header__right-content-container">
+      <div class="ui icon input">
+        <i class="search icon"></i>
+        <input type="text" placeholder="Search..."/>
+      </div>
+        <img className="header__right-content-container__logout" src="img/logout.svg" alt=""/>
+    </div>
   </div>
 );
 
--- src/screens/LeftBar.js
+++ src/screens/LeftBar.js
@@ -1,6 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+/*Mocked chats*/
+const messages = [{
+    sender: 'Rasmus Lerdorf',
+    messagePreview: 'Neque porro quisquam est, qui dolorem ipsum quia...',
+    unreadedMessages: 3
+  },{
+    sender: 'Mark Zuckerberg',
+    messagePreview: 'Neque porro quisquam est, qui dolorem ipsum quia...',
+    unreadedMessages: 2
+  },{
+    sender: 'Cláudia Bauzer',
+    messagePreview: 'Neque porro quisquam est, qui dolorem ipsum quia...',
+    unreadedMessages: 18
+  },{
+    sender: 'Eduardo Yuji',
+    messagePreview: 'Neque porro quisquam est, qui dolorem ipsum quia...',
+    unreadedMessages: 2
+  },{
+    sender: 'Zardél Santos',
+    messagePreview: 'Porra... Maneiro hein... Si Señor???',
+    unreadedMessages: 2
+  },{
+    sender: 'Douglas Santos',
+    messagePreview: 'Porra... Maneiro hein... Si Señor???',
+    unreadedMessages: 2
+  }]
+
+
+const Conversation = (props) => {
+    return (
+      <Link to="/">
+        <div class="left-bar-container__chat__conversation">
+          <div class="left-bar-container__chat__conversation__unreaded circleBase type1">
+            <p class="left-bar-container__chat__conversation__unreaded__number">{props.unreadedMessages}</p>
+          </div>
+          <div class="left-bar-container__chat__conversation__info">
+            <p class="left-bar-container__chat__conversation__info__sender">{props.sender}</p>
+            <p class="left-bar-container__chat__conversation__info__preview">{props.messagePreview}</p>
+          </div>
+        </div>
+      </Link>
+    );
+  }
+
 class LeftBar extends React.Component {
   render() {
     return (
@@ -14,6 +58,12 @@ class LeftBar extends React.Component {
               <p class="left-bar-container__profile__button__text">Editar perfil</p>
             </div>
           </Link>
+        </div>
+        <h1 className="left-bar-container__chat-title">Chat</h1>
+        <div className="left-bar-container__chat">
+          {messages.map(message =>
+            <Conversation sender={message.sender} messagePreview={message.messagePreview} unreadedMessages={message.unreadedMessages}/>
+          )}
         </div>
       </div>
     );
