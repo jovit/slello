3102821d7bad51df1f2c0e94480435a8525ad6fe
diego <diego.kamiha17@gmail.com>
2018-06-08 00:02:19 -0300

    Adding a sample chart, just to show how amazing this screen will be

--- src/screens/Charts.js
+++ src/screens/Charts.js
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 class Charts extends React.Component {
   render() {
     return (
-      <div className="center-component-container">
-        <div>
-          <h1 className="water-mark">Component Charts. Width: 756px<br/>Click on a widget and see the url change</h1>
+      <div className="charts-component-container">
+        <h1 className="charts-title">Gráficos</h1>
+        <div className="chart-body">
+          <h2 className="water-mark">Projeto Matthew Harris</h2>
+          <img src="img/chart1.png" className="chart1-style"/>
         </div>
       </div>
     );
