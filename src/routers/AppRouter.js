4352f6f358c73bf644846532ab2af9192d051573
João Vitor Araki Gonçalves <joao.goncalves@movile.com>
2018-06-27 23:12:08 -0300

    login home

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 /*Components*/
-import Home from '../screens/Home';
-import AddWidget from '../screens/AddWidget';
-import Charts from '../screens/Charts';
-import History from '../screens/History';
-import People from '../screens/People';
-import Group from '../screens/Group';
-import Header from '../screens/Header';
-import LeftBar from '../screens/LeftBar';
-import RightBar from '../screens/RightBar';
-import PagePeople from '../screens/People/PagePeople';
+import Home from "../screens/Home";
+import AddWidget from "../screens/AddWidget";
+import Charts from "../screens/Charts";
+import History from "../screens/History";
+import People from "../screens/People";
+import Group from "../screens/Group";
+import Header from "../screens/Header";
+import LeftBar from "../screens/LeftBar";
+import RightBar from "../screens/RightBar";
+import PagePeople from "../screens/People/PagePeople";
 import ProjectStatus from "../screens/gorfo/index";
 import ProjectInfo1 from "../screens/ProjectInfo_1";
 import ProjectInfo2 from "../screens/ProjectInfo_2";
 import ProjectInfo3 from "../screens/ProjectInfo_3";
 import ProjectInfo4 from "../screens/ProjectInfo_4";
-
 
 const AppRouter = () => {
   return (
@@ -27,16 +26,16 @@ const AppRouter = () => {
         <div id="browserRouterDiv">
           <LeftBar />
           <Switch>
-            <Route path="/" component={Home} exact={true}/>
-            <Route path="/charts" component={Charts}/>
-            <Route path="/group/:id" component={Group}/>
-            <Route path="/add-widget" component={People}/>
-            <Route path="/history" component={ProjectStatus}/>
-            <Route path="/projectinfo1" component={ProjectInfo1}/>
-            <Route path="/projectinfo2" component={ProjectInfo2}/>
-            <Route path="/projectinfo3" component={ProjectInfo3}/>
-            <Route path="/projectinfo4" component={ProjectInfo4}/>
-            <Route path="/people" component={PagePeople}/>
+            <Route path="/" component={Home} exact={true} />
+            <Route path="/charts" component={Charts} />
+            <Route path="/group/:id" component={Group} />
+            <Route path="/add-widget" component={People} />
+            <Route path="/history" component={ProjectStatus} />
+            <Route path="/projectinfo1" component={ProjectInfo1} />
+            <Route path="/projectinfo2" component={ProjectInfo2} />
+            <Route path="/projectinfo3" component={ProjectInfo3} />
+            <Route path="/projectinfo4" component={ProjectInfo4} />
+            <Route path="/people" component={PagePeople} />
           </Switch>
           <RightBar />
         </div>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,76 +1,97 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import axios from "axios";
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      token: ""
+    }
+  }
+
+  async componentDidMount() {
+    const headers = {
+      Authorization: "Basic am92aXQ6am92aXQ="
+    };
+    const response = await axios.post("https://gentle-thicket-23408.herokuapp.com/auth", undefined, {
+      headers
+    });
+    
+    this.setState({token: response.data.data.token})
+  }
+
   render() {
     return (
       <div className="home-component-container">
         <h1 className="home-title">Projetos</h1>
         <div className="projects-row">
-            <Link to="/projectinfo1">
-                <div className="home-card">
-                    <h2>Projeto Matthew Harris</h2>
-                    <p className="done-project-status">Finalizado</p>
-                    <p className="card-content">
-                      Início: 20/04/2018 <br />
-                      Fim: 30/05/2018
-                    </p>
-                  </div>
-            </Link>
-             <Link to="/projectinfo2">
-              <div className="home-card">
-                  <h2>Projeto Jake Smith</h2>
-                  <p className="wip-project-status">Em andamento</p>
-                  <p className="card-content">
-                    Início: 30/05/2018 <br />
-                    Fim previsto: 20/06/2020
-                  </p>
-              </div>
-            </Link>
-        </div>
-         <div className="projects-row">
-            <Link to="/projectinfo3">
-                <div className="home-card">
-                    <h2>Projeto Slello</h2>
-                    <p className="wip-project-status">Em andamento</p>
-                    <p className="card-content">
-                      Início: 20/04/2018 <br />
-                      Fim previsto: 30/05/2055
-                    </p>
-                  </div>
-            </Link>
-             <Link to="/projectinfo4">
-              <div className="home-card">
-                  <h2>IC 4</h2>
-                  <p className="wip-project-status">Em andamento</p>
-                  <p className="card-content">
-                    Início: 30/05/2018 <br />
-                    Fim previsto: 20/06/2088
-                  </p>
-              </div>
-            </Link>
+          <Link to="/projectinfo1">
+            <div className="home-card">
+              <h2>Projeto Matthew Harris</h2>
+              <p className="done-project-status">Finalizado</p>
+              <p className="card-content">
+                Início: 20/04/2018 <br />
+                Fim: 30/05/2018
+              </p>
+            </div>
+          </Link>
+          <Link to="/projectinfo2">
+            <div className="home-card">
+              <h2>Projeto Jake Smith</h2>
+              <p className="wip-project-status">Em andamento</p>
+              <p className="card-content">
+                Início: 30/05/2018 <br />
+                Fim previsto: 20/06/2020
+              </p>
+            </div>
+          </Link>
         </div>
         <div className="projects-row">
-            <Link to="/projectinfo1">
-                <div className="home-card">
-                    <h2>Windows Phone</h2>
-                    <p className="canceled-project-status">Cancelado</p>
-                    <p className="card-content">
-                      Início: 08/11/2010 <br />
-                      Fim: 09/11/2010
-                    </p>
-                  </div>
-            </Link>
-             <Link to="/projectinfo2">
-               <div className="home-card">
-                  <img src="img/selected_add_widget.svg" className="add-image"/>
-                  <h2>Adicionar Projeto</h2>
-              </div>
-            </Link>
+          <Link to="/projectinfo3">
+            <div className="home-card">
+              <h2>Projeto Slello</h2>
+              <p className="wip-project-status">Em andamento</p>
+              <p className="card-content">
+                Início: 20/04/2018 <br />
+                Fim previsto: 30/05/2055
+              </p>
+            </div>
+          </Link>
+          <Link to="/projectinfo4">
+            <div className="home-card">
+              <h2>IC 4</h2>
+              <p className="wip-project-status">Em andamento</p>
+              <p className="card-content">
+                Início: 30/05/2018 <br />
+                Fim previsto: 20/06/2088
+              </p>
+            </div>
+          </Link>
+        </div>
+        <div className="projects-row">
+          <Link to="/projectinfo1">
+            <div className="home-card">
+              <h2>Windows Phone</h2>
+              <p className="canceled-project-status">Cancelado</p>
+              <p className="card-content">
+                Início: 08/11/2010 <br />
+                Fim: 09/11/2010
+              </p>
+            </div>
+          </Link>
+          <Link to="/projectinfo2">
+            <div className="home-card">
+              <img src="img/selected_add_widget.svg" className="add-image" />
+              <h2>Adicionar Projeto</h2>
+            </div>
+          </Link>
         </div>
       </div>
     );
-  };
-};
+  }
+}
 
 export default Home;
