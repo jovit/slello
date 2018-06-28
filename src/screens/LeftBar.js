d767f7edf754509b07fff4386ea7b7d5c914ba01
João Vitor Araki Gonçalves <joao.goncalves@movile.com>
2018-06-28 18:34:00 -0300

    chat

--- src/screens/LeftBar.js
+++ src/screens/LeftBar.js
@@ -1,73 +1,203 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-/*Mocked chats*/
-const messages = [{
-    sender: 'Rasmus Lerdorf',
-    messagePreview: 'Neque porro quisquam est, qui dolorem ipsum quia...',
-    unreadedMessages: 3
-  },{
-    sender: 'Mark Zuckerberg',
-    messagePreview: 'Neque porro quisquam est, qui dolorem ipsum quia...',
-    unreadedMessages: 2
-  },{
-    sender: 'Cláudia Bauzer',
-    messagePreview: 'Neque porro quisquam est, qui dolorem ipsum quia...',
-    unreadedMessages: 18
-  },{
-    sender: 'Eduardo Yuji',
-    messagePreview: 'Neque porro quisquam est, qui dolorem ipsum quia...',
-    unreadedMessages: 2
-  },{
-    sender: 'Zardél Santos',
-    messagePreview: 'Porra... Maneiro hein... Si Señor???',
-    unreadedMessages: 2
-  },{
-    sender: 'Douglas Santos',
-    messagePreview: 'Porra... Maneiro hein... Si Señor???',
-    unreadedMessages: 2
-  }]
+class Conversation extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      token: "",
+      expanded: false,
+      messages: [],
+      messageInput: ""
+    };
+  }
 
-const Conversation = (props) => {
+  async componentDidMount() {
+    const headers = {
+      Authorization: "Basic am92aXQ6am92aXQ="
+    };
+    const response = await axios.post(
+      "https://gentle-thicket-23408.herokuapp.com/auth",
+      null,
+      {
+        headers
+      }
+    );
+
+    const token = response.data.data.token;
+
+    this.setState({ token: token });
+
+    const messagesResponse = await axios.get(
+      "https://gentle-thicket-23408.herokuapp.com/latest/messages?channel=" +
+        this.props.name,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
+
+    this.setState({ messages: messagesResponse.data });
+  }
+
+  async sendMessage() {
+    const message = this.state.messageInput
+
+    this.setState({ messageInput: "" });
+
+    const messagesResponse = await axios.post(
+      "https://gentle-thicket-23408.herokuapp.com/latest/messages",
+      {
+        destination: this.props.name,
+        message,
+        type: "CHAT_MESSAGE"
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${this.state.token}`
+        }
+      }
+    );
+
+    const messageResponse = await axios.get(
+      "https://gentle-thicket-23408.herokuapp.com/latest/messages?channel=" +
+        this.props.name,
+      {
+        headers: {
+          Authorization: `Bearer ${this.state.token}`
+        }
+      }
+    );
+
+    this.setState({ messages: messageResponse.data });
+
+  }
+
+  render() {
     return (
       <Link to="/">
-        <div class="left-bar-container__chat__conversation">
-          <div class="left-bar-container__chat__conversation__unreaded circleBase type1">
-            <p class="left-bar-container__chat__conversation__unreaded__number">{props.unreadedMessages}</p>
+        <div className="left-bar-container__chat__conversation">
+          <div className="left-bar-container__chat__conversation__unreaded circleBase type1">
+            <p className="left-bar-container__chat__conversation__unreaded__number">
+              {this.props.unreadedMessages}
+            </p>
           </div>
-          <div class="left-bar-container__chat__conversation__info">
-            <p class="left-bar-container__chat__conversation__info__sender">{props.sender}</p>
-            <p class="left-bar-container__chat__conversation__info__preview">{props.messagePreview}</p>
+          <div
+            className="left-bar-container__chat__conversation__info"
+            onClick={() => this.setState({ expanded: !this.state.expanded })}
+          >
+            <p className="left-bar-container__chat__conversation__info__sender">
+              {this.props.name}
+            </p>
           </div>
+        </div>
+        <div style={{ fontSize: "16px" }}>
+          {this.state.expanded
+            ? this.state.messages.map(message => (
+                <p
+                  key={message.id.timestamp}
+                  className="left-bar-container__chat__conversation__info__preview"
+                  style={{ fontSize: "16px" }}
+                >
+                  <strong>{message.user}:</strong> {message.message}
+                </p>
+              ))
+            : null}
+          {this.state.expanded && (
+            <input
+              value={this.state.messageInput}
+              onChange={e => this.setState({ messageInput: e.target.value })}
+              style={{ width: "100%" }}
+              type="text"
+              placeholder="Enviar mensagem"
+              onKeyPress={e => (e.key === "Enter" ? this.sendMessage() : null)}
+            />
+          )}
         </div>
       </Link>
     );
   }
+}
 
 class LeftBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      token: "",
+      channels: []
+    };
+  }
+
+  async componentDidMount() {
+    const headers = {
+      Authorization: "Basic am92aXQ6am92aXQ="
+    };
+    const response = await axios.post(
+      "https://gentle-thicket-23408.herokuapp.com/auth",
+      null,
+      {
+        headers
+      }
+    );
+
+    const token = response.data.data.token;
+
+    this.setState({ token: token });
+
+    const channelsResponse = await axios.get(
+      "https://gentle-thicket-23408.herokuapp.com/latest/channels",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
+
+    this.setState({ channels: channelsResponse.data.data });
+  }
+
   render() {
     return (
       <div class="left-bar-container">
         <div className="left-bar-container__profile">
-          <img className="left-bar-container__profile__picture" src="img/man.svg" alt=""/>
-          <p class="left-bar-container__profile__name">Fausto Silva<br/>Dev Front End</p>
+          <img
+            className="left-bar-container__profile__picture"
+            src="img/man.svg"
+            alt=""
+          />
+          <p class="left-bar-container__profile__name">
+            Fausto Silva<br />Dev Front End
+          </p>
           <Link to="/">
             <div class="left-bar-container__profile__button">
-              <img className="left-bar-container__profile__button__picture" src="img/pencil.svg" alt=""/>
-              <p class="left-bar-container__profile__button__text">Editar perfil</p>
+              <img
+                className="left-bar-container__profile__button__picture"
+                src="img/pencil.svg"
+                alt=""
+              />
+              <p class="left-bar-container__profile__button__text">
+                Editar perfil
+              </p>
             </div>
           </Link>
         </div>
         <h1 className="left-bar-container__chat-title">Chat</h1>
         <div className="left-bar-container__chat">
-          {messages.map(message =>
-            <Conversation sender={message.sender} messagePreview={message.messagePreview} unreadedMessages={message.unreadedMessages}/>
-          )}
+          {this.state.channels.map(channel => (
+            <Conversation
+              key={channel.name}
+              name={channel.name}
+              unreadedMessages={channel.members.length}
+            />
+          ))}
         </div>
       </div>
     );
-  };
-};
+  }
+}
 
 export default LeftBar;
